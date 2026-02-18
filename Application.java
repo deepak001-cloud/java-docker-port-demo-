30916c5bc1400619040c218f913dcd3451ac13af
deepak001-cloud <144651562+deepak001-cloud@users.noreply.github.com>
2026-02-18 10:31:01 +0530

    Create Spring Boot application with a greeting endpoint

---
+++ Application.java
@@ -0,0 +1,19 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Deepak! Jenkins + Docker is working on PORT 8081 🚀";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
